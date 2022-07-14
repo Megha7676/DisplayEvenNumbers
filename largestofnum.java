e3c6986b99b0d754a0e8f29f759bf0d3bbd6ca57
Megha7676 <107993247+Megha7676@users.noreply.github.com>
2022-07-14 10:14:25 +0530

    Add files via upload

---
+++ largestofnum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class largestofnum{
+public static void main(String args[])
+{
+Scanner so=new Scanner(System.in);
+System.out.print("Enter the first number:");
+int firstNum=so.nextInt();
+System.out.print("Enter the second number:");
+int secondNum=so.nextInt();
+if(firstNum>secondNum)
+{
+System.out.println(firstNum+" is largest");
+}
+else{
+System.out.println(secondNum+" is largest");
+}
+}
+}
